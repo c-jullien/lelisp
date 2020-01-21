222a0342367fc04b14938514e8e1d3dd5b3e46c7
U-COLTRANE\jullien <jullien@coltrane.eligis.com>
2020-01-21 15:50:10 +0100

    Update x86macos C files

--- x86macos/C/Execore.c
+++ x86macos/C/Execore.c
@@ -1,7 +1,7 @@
 
 /* Maintenance: ce fichier est engendre automatiquement */
 /*                                                      */
-/* $Id: Execore.c,v 1.3 2017/07/08 07:05:46 jullien Exp $ */
+/* $Id$ */
 /********************************************************/
 
 /* pas de mode EXECORE sur cette machine */
